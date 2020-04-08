f675a75c6298bf84ec23ffa3cbd7622a4335ba55
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-04-08 17:32:01 -0300

    add

---
+++ Java Projects/1048 - Técnicas de Programação - Herança - Agência Bancária/src/Cliente.java
@@ -0,0 +1,31 @@
+
+public class Cliente {
+	private int cpf;
+	private String nome;
+	private String telefone;
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
