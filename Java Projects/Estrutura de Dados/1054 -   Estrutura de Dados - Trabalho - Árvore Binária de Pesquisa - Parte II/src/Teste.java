7c8f040a45bf92abcceb5fb44e67ee1e5fa372ca
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-05-20 13:24:24 -0300

    add

--- Java Projects/Estrutura de Dados/1054 -   Estrutura de Dados - Trabalho - Árvore Binária de Pesquisa - Parte II/src/Teste.java
+++ Java Projects/Estrutura de Dados/1054 -   Estrutura de Dados - Trabalho - Árvore Binária de Pesquisa - Parte II/src/Teste.java
@@ -7,24 +7,28 @@ public class Teste {
 		String pNome, pFone;
 		System.out.print(" Digite o nome: ");
 		pNome = leia.next();
-		System.out.print(" Digite o fone: ");
-		pFone = leia.next();
-		Item dados = new Item(pNome, pFone);
+		//System.out.print(" Digite o fone: ");
+		//pFone = leia.next();
+		Item dados = new Item(pNome, "12345");
 		return dados;
 
 	}
 
 	public static int Escolha() {
 		System.out.println("\n----------------------");
-		System.out.println(" 1. Inserir Dados");
-		System.out.println(" 2. Pesquisar ");
-		System.out.println(" 3. Retirar");
-		System.out.println(" 4. Ver Nó Maximo e Mínimo");
-		System.out.println(" 5. Antecessor e Sucessor de um Nó");
-		System.out.println(" 6. Visita em Ordem");
-		System.out.println(" 7. Visita em pré Ordem");
-		System.out.println(" 8. Visita em pós Ordem");
-		System.out.println(" 9. Testa Integridade");
+		System.out.println(" 1.  Inserir Dados");
+		System.out.println(" 2.  Pesquisar ");
+		System.out.println(" 3.  Retirar");
+		System.out.println(" 4.  O maior valor da Árvore");
+		System.out.println(" 5.  O menor valor da Árvore");
+		System.out.println(" 6.  O antecessor de um Nó");
+		System.out.println(" 7.  O sucessor de um Nó");
+		System.out.println(" 8.  Visita em Ordem");
+		System.out.println(" 9.  Visita em pré Ordem");
+		System.out.println(" 10. Visita em pós Ordem");
+		System.out.println(" 11. Testa Integridade");
+		System.out.println(" 12. Conta quantidade de Folhas");
+		System.out.println(" 13. Conta total de Nós");
 		System.out.println(" 0. Sair\n");
 		System.out.print(" Escolha: ");
 		int escolha = leia.nextInt();
@@ -58,53 +62,50 @@ public class Teste {
 				if (aux.pesquisar(aRetirar) == null) System.out.println("\n -- Valor não Encontrado -- \n");
 				else System.out.println("\n Valor Removido: \n " + aux.retirar(aRetirar));
 				break;
-			
 			case 4:
+				aux.getMaximo();
 				break;
-			
 			case 5:
+				
 				break;
-			
-			case 6:
+			case 8:
 				sb = new StringBuffer();
 				System.out.print(" Digite um nome para ser visitado em Ordem: ");
 				sb.append(leia.next());
 				aux.visitaEmOrdem(sb);
 				break;
-			
-			case 7:
+			case 9:
 				sb = new StringBuffer();
 				System.out.print(" Digite um nome para ser visitado em Pré Ordem: ");
 				sb.append(leia.next());
 				aux.visitaEmPreOrdem(sb);
 				break;
-			
-			case 8:
+			case 10:
 				sb = new StringBuffer();
 				System.out.print(" Digite um nome para ser visitado em Pós Ordem: ");
 				sb.append(leia.next());
 				aux.visitaEmPosOrdem(sb);
 				break;
-			
-			case 9:
+			case 11:
 				sb = new StringBuffer();
 				System.out.print(" Digite um nome para Testar sua Integridade na Árvore: ");
 				sb.append(leia.next());
 				aux.testaIntegridade(sb);
 				break;				
-			
-			case 0:
-				System.exit(0);
+			case 12:
+				System.out.println(aux.getContaFolhas());
+				break;
+			case 13:
+				System.out.println(aux.getContaNos());
+				break;
 				
 
 			}
 
-		} while (escolha != 8);
+		} while (escolha != 0);
 	}
 
 }
-
-// ver: https://gist.github.com/divanibarbosa/819e7cfcf1b9bae48c4e0f5bd74fb658
 
 
 
