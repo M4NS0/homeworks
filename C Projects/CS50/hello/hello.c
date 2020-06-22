1f5c5e855aeea3324921c1cd5ea13f5d5ba33b2d
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-06-22 15:16:18 -0300

    add

---
+++ C Projects/CS50/hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+// clang hello.c    // to compile
+// ./a.out          // testing compiled
+// make hello       // make it executable
+// ./hello          // to run executable
+
