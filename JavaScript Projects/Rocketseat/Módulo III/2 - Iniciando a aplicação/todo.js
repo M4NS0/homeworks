40f795b71e13a101e1f1062bb5beec099a388ef5
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-06-23 16:17:38 -0300

    add

---
+++ JavaScript Projects/Rocketseat/Módulo III/2 - Iniciando a aplicação/todo.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var listElement = document.querySelector('#app input');
+var listElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer Café',
+    'Estudar JavaScript',
+    'Acessar C.V.'
+];
