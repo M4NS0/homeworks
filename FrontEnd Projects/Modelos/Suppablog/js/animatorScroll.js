e63ba9d8211d420cd911fccc72c90dc0481b2bc4
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-06-27 12:53:47 -0300

    add

---
+++ FrontEnd Projects/Modelos/Suppablog/js/animatorScroll.js
@@ -0,0 +1,9 @@
+$('.entry-content .content-wrap content-660 center-relative a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
