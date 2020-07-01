8fd3faf48af2d975ca4a663414ffa5225afba41d
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-07-01 14:47:10 -0300

    add

--- Web/Cursos Rocketseat/Starter/es6/src/main.js
+++ Web/Cursos Rocketseat/Starter/es6/src/main.js
@@ -1,1 +1,7 @@
-alert('Teste');
+// lê se '=>' que retorna
+const minhaPromisse = () => new Promisse((resolve, reject) => { setTimeout(() => { resolve('Ok') }, 2000);
+});
+
+minhaPromisse.then(response => {
+    console.log(response);
+});
