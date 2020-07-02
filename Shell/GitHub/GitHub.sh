8a8c66d5934f18d21daec224058b6a9843f35150
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-07-02 16:18:25 -0300

    Create - Escreva um comentário adicional

---
+++ Shell/GitHub/GitHub.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+repName=$(echo "Workspaces,Matriz.Curricular,Projetos,M4ns0.github.io")
+crud=$(echo "Create,Remove,Update,Delete")
+yad --title="Nome do Repositório" \
+--form --separator="," --item-separator="," \
+--field="Nome do Repositório:CB" \
+--field="CRUD:CBE" \
+--field="Comments:TXT" \
+"$repName" "$crud" "Escreva um comentário adicional" > config
+
+NAME=`cat config | awk -F',' '{print $1}'`
+CRUD=`cat config | awk -F',' '{print $2}'`
+COMMENT=`cat config | awk -F',' '{print $3}'`
+
+cd ~/$USR/$NAME
+git add .
+git commit -m "$CRUD - $COMMENT"
+
+pwd
+
+echo $NAME $CRUD $COMMENT
+
