73378f21e7f489d5e4d56b322b3b6161efc36370
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-07-13 11:47:48 -0300

    remove

--- .vscode/Testes/testeJava.java
+++
@@ -1,8 +0,0 @@
-// para rodar o programa use control shift b
-
-
-public class testeJava {
-    public static void main(String[ ]args) {
-        System.out.println("Hello World");
-    }
-}
--- .vscode/Testes/testePython.py
+++
@@ -1,4 +0,0 @@
-print("hello you")
-print("hey")
-print("Ho")
-# f5 ou f6 para rodar o script em python no Atom
