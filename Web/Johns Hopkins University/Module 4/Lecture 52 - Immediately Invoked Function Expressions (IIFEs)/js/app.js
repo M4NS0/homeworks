fe7291d3ffd0519475034fe219f9e2d80373562a
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-08-07 10:42:42 -0300

    Update

--- Web/Johns Hopkins University/Module 4/Lecture 52 - Immediately Invoked Function Expressions (IIFEs)/js/app.js
+++ Web/Johns Hopkins University/Module 4/Lecture 52 - Immediately Invoked Function Expressions (IIFEs)/js/app.js
@@ -2,6 +2,34 @@
 sayHello();
 sayHi();
 
+yaakovGreeter.sayHello();
+brunoGreeter.sayHi();
+
 */
+
+function a() {
+    console.log("Hello World")
+}
+a();
+
+
+
+var a = (function() {
+    console.log("Hello World")
+});
+a();
+
+
+// Immediately Invoked Function Expression
+// IIFE
+
+(function() {
+    console.log("Hello World")
+})();
+
+(function(something) {
+    console.log("Hello " + something);
+})("World!");
+
 yaakovGreeter.sayHello();
 brunoGreeter.sayHi();
--- Web/Johns Hopkins University/Module 4/Lecture 52 - Immediately Invoked Function Expressions (IIFEs)/js/script2.js
+++ Web/Johns Hopkins University/Module 4/Lecture 52 - Immediately Invoked Function Expressions (IIFEs)/js/script2.js
@@ -4,7 +4,7 @@ var name = "Bruno";
 function sayHi() {
     console.log("Hi " + name);
 }
-*/
+
 
 var brunoGreeter = {};
 brunoGreeter.name = "Bruno";
@@ -14,3 +14,16 @@ brunoGreeter.sayHi = function() {
     console.log(greeting + brunoGreeter.name);
 
 }
+*/
+
+(function(window) {
+    var brunoGreeter = {};
+    brunoGreeter.name = "Bruno";
+    var greeting = "Hi ";
+
+    brunoGreeter.sayHi = function() {
+        console.log(greeting + brunoGreeter.name);
+
+    }
+    window.brunoGreeter = brunoGreeter;
+})(window);
