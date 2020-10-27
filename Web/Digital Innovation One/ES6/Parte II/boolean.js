a35876842e09fa765f5ac1de805e1cb82d7419b9
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-10-27 18:32:10 -0300

    Update

---
+++ Web/Digital Innovation One/ES6/Parte II/boolean.js
@@ -0,0 +1,4 @@
+const isActive = true;
+const isAuthenticated = false;
+
+console.log(`O tipo de variável de "isActive" é ${typeof isActive}`);
---
+++ Web/Digital Innovation One/ES6/Parte II/null.js
@@ -0,0 +1,4 @@
+const nullVariable = null;
+
+console.log('Variável nula' + 
+            `\nO tipo da variável nullVariable é: ${typeof nullVariable}`);
