c22608b168149d181c638d4ec532d487f67e6b37
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-11-01 10:19:04 -0300

    Update

--- Web/Desenvolvimento de Software Web/ApiClientes/src/main/java/br/edu/pucgoias/apiclientes/controller/ClienteControler.java
+++ Web/Desenvolvimento de Software Web/ApiClientes/src/main/java/br/edu/pucgoias/apiclientes/controller/ClienteControler.java
@@ -22,13 +22,12 @@ import br.edu.pucgoias.apiclientes.model.Cliente;
 import br.edu.pucgoias.apiclientes.repository.ClienteRepository;
 import br.edu.pucgoias.apiclientes.services.ClienteServices;
 
-// api recebe um endpoint e faz com q a api direcione para o método certo
-// pra onde é chamado as chamadas de endpoint
-
-@RestController // Permite retornar informações apresentadas em uma view. 
-				// Retorna ou uma resposta JSon ou uma resposta XML
-				// Recebe dados de clientes via JSon
-
+																			// Api recebe um endpoint e faz com q a api direcione para o método certo
+																			// Pra onde é chamado as chamadas de endpoint
+																			// Mapeia as rotas
+															 				// Permite retornar informações apresentadas em uma view. 
+																			// Retorna ou uma resposta JSon ou uma resposta XML
+@RestController																// Recebe dados de clientes via JSon
 @RequestMapping("/clientes") 
 public class ClienteControler {
 	
@@ -38,31 +37,36 @@ public class ClienteControler {
 	@Autowired
 	private ClienteServices clienteService;
 	
-	@GetMapping // Essem metodo vai ser acessado pelo getMapping, pegando o /clientes direcionando pra ele
+																			// Essem metodo vai ser acessado pelo getMapping, 
+	@GetMapping 															// pegando o /clientes direcionando pra ele
 	public List<Cliente> listar() {
-		return clienteRepository.findAll(); // retorna array list
-											// retorna coleção de objetos json
-	}
+		return clienteRepository.findAll(); 								// Retorna um array list
+	}																		// Retorna coleção de objetos no formato json
+																			// sem a necessidade de usar o Gson
 	
-	@GetMapping("/{clienteId}") // usa o clienteID dessa vez, passa um parametro pra ele,  o endipoint sera /clientesId
-	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
-		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+																			// usa o clienteID dessa vez, passa um parametro pra ele
+																			// o endipoint sera /clientesId
+	@GetMapping("/{clienteId}") 											// consulta o cliente pelo codigo
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) { 	// passa variavel pelo caminho
 		
-		if (cliente.isPresent()) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);  // Retorna opcionalmente o objeto, quando houver o cliente com o código passado
+																			// pesquisar JPQL que é usado dentro do hibernate, que não usa as classes
+		if (cliente.isPresent()) {											// se achou retorna o método Response entity com ok de resposta
 			return ResponseEntity.ok(cliente.get());
 		}
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.notFound().build();							// se não, retorna um não encontrado
 	}
 	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
-		return clienteService.salvar(cliente);
+	@PostMapping															// Busca o tipo de ação
+	@ResponseStatus(HttpStatus.CREATED)										// cria um novo registro
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {			// passado pelo @RequestMapping
+																			// Valida variável no path no corpo onde esta o obj passado
+		return clienteService.salvar(cliente);								// método dentro do jpa repository
 	}
 	
-	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId,
-			@RequestBody Cliente cliente) {
+	@PutMapping("/{clienteId}")												// Passa um cógigo para alterar /clientes + /código
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable 			// Valida variável no path no corpo onde esta o obj passado
+			Long clienteId, @RequestBody Cliente cliente) {
 		if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
@@ -72,7 +76,7 @@ public class ClienteControler {
 		return ResponseEntity.ok(cliente);
 		
 	}
-	@DeleteMapping("/{clienteId}")
+	@DeleteMapping("/{clienteId}")											// Chamado com / cliente + /código exclui o cliente
 	public ResponseEntity<Void> remover (@PathVariable Long clienteId) {
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
@@ -80,6 +84,4 @@ public class ClienteControler {
 		clienteService.excluir(clienteId);
 		return ResponseEntity.noContent().build();
 	}
-	
-	
 }
