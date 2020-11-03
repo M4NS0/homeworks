0975ebfffe497a8b9df866ae3c590a50fea448ce
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-11-03 11:36:29 -0300

    Update

---
+++ Web/Udemy/Angular The Complete Guide (2020)/SecondApp/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+// component is a typescript class
+
+import { Component } from '@angular/core';
+
+@Component({
+    // will be stored in metadata
+    selector:  'app-server',    // the html tag, unique selector
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
