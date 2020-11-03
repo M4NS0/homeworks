e3100b2cc1bab1e81ad6ecd18e24a24d38b797c4
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-11-03 11:14:01 -0300

    git ignoring angular stuffs

--- Web/Udemy/Angular The Complete Guide (2020)/SecondApp/karma.conf.js
+++ Web/Udemy/Angular The Complete Guide (2020)/SecondApp/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/my-second-app'),
+      dir: require('path').join(__dirname, './coverage/SecondApp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
