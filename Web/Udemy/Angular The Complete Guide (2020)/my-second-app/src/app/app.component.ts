945b1fb32c152e46ed7311df07879504a5b77e2f
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-11-03 10:33:18 -0300

    Update

--- Web/Udemy/Angular The Complete Guide (2020)/my-second-app/src/app/app.component.ts
+++ Web/Udemy/Angular The Complete Guide (2020)/my-second-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',  // assigns the string 'selector' as a value named 'app-root'
+  // assigning the string 'selector' as a value named 'app-root':
+  selector: 'app-root',  
+  
+   // assigning the template:
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
 
+  // styleUrls: ['./app.component.css']  // (old line)
+  // stylesheets reference, passing attributes from app.components.css through server.component.html
+  // could add pointers to another stylesheets styleUrls: ['./app.component.css', './anotherStylesheetComponent']
+  // or it allows to add styles right here, using an array of css lines
+  // in this way, it will override the app.component.css document, giving it priority, as bellow:                                   
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
+})
+
+export class AppComponent {
 }
