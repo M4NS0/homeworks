fbf884170f73a4e0d951484191f0480530870614
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-12-17 06:46:04 -0300

    add

--- Web/Udemy/The Complete Angular Course: Beginner to Advanced/Angular Fundamentals/course-app/src/app/app.module.ts
+++ Web/Udemy/The Complete Angular Course: Beginner to Advanced/Angular Fundamentals/course-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { SummaryPipe } from './summary.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -8,17 +8,20 @@ import { CourseComponent } from './course/course.component'; // auto generated b
 import { CoursesService } from './course/courses.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [    // here we add all the components
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent  // this component was auto generated using: ng g c course
+    AuthorsComponent,  // this component was auto generated using: ng g c course
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
                 // Part of dependency injection:
   providers: [  // Adding the provider, to dependency injection works
--- Web/Udemy/The Complete Angular Course: Beginner to Advanced/Angular Fundamentals/course-app/src/app/courses.component.ts
+++ Web/Udemy/The Complete Angular Course: Beginner to Advanced/Angular Fundamentals/course-app/src/app/courses.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { $ } from 'protractor';
 import { CoursesService } from './course/courses.service';
+
 
 
 
@@ -51,14 +51,54 @@ import { CoursesService } from './course/courses.service';
 
     <br/>
     <input #email (keyup.enter)="keyUpEmail(email.value)"/> Template Variables
+
+    <!-- 
+    <input (value)="NewMail" (keyup.enter)="NewMail = $event.target.value; twoWaykeyUpEmail();"/> Two-Way Binding
+    -->
+
+    <br/>
+    <input [(ngModel)]="newMail" (keyup.enter)="twoWaykeyUpEmail()"/> Two-Way Binding
+    <!-- [()] this is a banana in the box XD  -->
+
+    
+    <div>
+        <ul style="list-style-type:none;margin-top:50px">
+            <li>Name: {{ course.title | uppercase | lowercase}} </li>
+            <li>Rating: {{ course.rating | number:'1.2-2' }} </li>
+            <li>Rating: {{ course.rating | number:'1.1-1' }} rounded </li>
+            <li>Rating: {{ course.rating | number:'2.1-1' }} (left zero adding) </li>
+            <li>Students:  {{ course.students | number }} </li>
+            <li>Price:  {{ course.price | currency:'BRL':true:'3.2-2' }} </li>
+            <li>Release Date: {{ course.releaseDate | date:'shortDate'}} </li>
+        </ul>
+    </div>
+
+    {{ text | summary }}
     `
 })
 
 
 export class CoursesComponent {
+    imageUrl = "https://www.geteducated.com/wp-content/uploads/2019/11/online-tutoring-concept-ebooks-internet-courses-process-vector-staff-vector-id1053519062.jpg";
+    colSpan = 2;
+    courses;
+    email;
+    newMail = "bruno@example.com"
     isActive = false;
     title = "List of Courses";
-    
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016,3,1)
+    }
+    text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae lacinia risus. Pellentesque a felis tortor.`
+
+    twoWaykeyUpEmail() {
+        window.alert(this.newMail); 
+    } 
+
     keyUpEmail(email) {
         window.alert(email)
     }
@@ -86,10 +126,6 @@ export class CoursesComponent {
         window.alert("Button was clicked");
         console.log($event)
     }
-
-    imageUrl = "https://www.geteducated.com/wp-content/uploads/2019/11/online-tutoring-concept-ebooks-internet-courses-process-vector-staff-vector-id1053519062.jpg";
-    colSpan = 2;
-    courses;
        constructor(service: CoursesService) {
         this.courses = service.getCourses();
     }
