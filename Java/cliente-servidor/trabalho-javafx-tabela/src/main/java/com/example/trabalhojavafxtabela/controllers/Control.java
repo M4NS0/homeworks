8a83a0db0bc49879141cf15e3c74b078b3876eb9
Bruno C. Manso <mrbrunomanso@gmail.com>
2021-11-15 21:07:36 -0300

    Update

--- Java/cliente-servidor/trabalho-javafx-tabela/src/main/java/com/example/trabalhojavafxtabela/controllers/Control.java
+++ Java/cliente-servidor/trabalho-javafx-tabela/src/main/java/com/example/trabalhojavafxtabela/controllers/Control.java
@@ -1,14 +1,82 @@
 package com.example.trabalhojavafxtabela.controllers;
 
+import com.example.trabalhojavafxtabela.models.PessoaFisica;
+import com.example.trabalhojavafxtabela.models.Telefone;
+import com.example.trabalhojavafxtabela.persistence.DaoPessoaFisica;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.w3c.dom.events.MouseEvent;
 
-public class Control {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Control implements Initializable {
+
     @FXML
-    private TableColumn<, ?> idCol;
+    private TableColumn<PessoaFisica, Integer> idCol;
 
     @FXML
-    private TableColumn<?, ?> nomeCol;
+    private TableColumn<PessoaFisica, String> nomeCol;
 
+    @FXML
+    private TableView<PessoaFisica> tabelaPessoa;
+
+    @FXML
+    private TableColumn<Telefone, String> celularCol;
+
+    @FXML
+    private TableColumn<PessoaFisica, String> cidadeCol;
+
+    @FXML
+    private TableColumn<Telefone, String> comercialCol;
+
+    @FXML
+    private TableColumn<Telefone, String> contatoCol;
+
+    @FXML
+    private TableColumn<PessoaFisica, String> cpfCol;
+
+    @FXML
+    private TableColumn<PessoaFisica, String> estadoCol;
+
+
+    @FXML
+    private TableColumn<?, ?> statusCol;
+    private ObservableList<PessoaFisica> personData = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getDataInFile();
+        addInTabela();
+
+        tabelaPessoa.setItems(personData);
+    }
+
+    private void addInTabela() {
+        idCol.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
+        nomeCol.setCellValueFactory(cellData -> cellData.getValue().nomeProperty());
+        cpfCol.setCellValueFactory(cellData -> cellData.getValue().cpfProperty());
+        cidadeCol.setCellValueFactory(cellData -> cellData.getValue().cidadeProperty());
+        estadoCol.setCellValueFactory(cellData -> cellData.getValue().estadoProperty());
+//        celularCol.setCellValueFactory(cellData -> cellData.getValue().celularProperty());
+//        contatoCol.setCellValueFactory(cellData -> cellData.getValue().contatoProperty());
+//        comercialCol.setCellValueFactory(cellData -> cellData.getValue().comercialProperty());
+//        statusCol.setCellValueFactory(cellData -> cellData.getValue().statusProperty());
+    }
+
+    private void getDataInFile() {
+        DaoPessoaFisica dao = new DaoPessoaFisica();
+        List<PessoaFisica> list;
+        list = dao.getAll();
+        personData.addAll(list);
+
+    }
 
 }
--- Java/cliente-servidor/trabalho-javafx-tabela/src/main/java/com/example/trabalhojavafxtabela/models/PessoaFisica.java
+++ Java/cliente-servidor/trabalho-javafx-tabela/src/main/java/com/example/trabalhojavafxtabela/models/PessoaFisica.java
@@ -1,4 +1,103 @@
 package com.example.trabalhojavafxtabela.models;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
 
 public class PessoaFisica {
+    private IntegerProperty id;
+    private StringProperty cpf, nome, cidade, estado, status;
+    private List<Telefone> telefones;
+
+    public PessoaFisica() {
+        this.id = new SimpleIntegerProperty();
+        this.cpf = new SimpleStringProperty();
+        this.nome = new SimpleStringProperty();
+        this.cidade = new SimpleStringProperty();
+        this.estado = new SimpleStringProperty();
+        this.status = new SimpleStringProperty();
+        this.telefones = new SimpleListProperty<>();
+    }
+
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getCpf() {
+        return cpf.get();
+    }
+
+    public StringProperty cpfProperty() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf.set(cpf);
+    }
+
+    public String getNome() {
+        return nome.get();
+    }
+
+    public StringProperty nomeProperty() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome.set(nome);
+    }
+
+    public String getCidade() {
+        return cidade.get();
+    }
+
+    public StringProperty cidadeProperty() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade.set(cidade);
+    }
+
+    public String getEstado() {
+        return estado.get();
+    }
+
+    public StringProperty estadoProperty() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado.set(estado);
+    }
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
 }
